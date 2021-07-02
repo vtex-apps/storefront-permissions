a04a639ed1a4d04ab8412526a88b4dbf405ecfa5
Wender Lima <wender.lima@vtex.com.br>
2021-07-02 18:05:22 -0300

    Now using CL users from admin-customers-graphql

--- node/utils/LicenseManager.ts
+++ node/utils/LicenseManager.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { InstanceOptions, IOContext } from '@vtex/api'
 import { ExternalClient } from '@vtex/api'
@@ -15,68 +14,66 @@ export class LMClient extends ExternalClient {
         Accept: 'application/json',
       },
     })
-    console.log('ctx.authToken =>', ctx.authToken)
   }
 
-  private createRole = () => {
-    const data: any = {
-      name: 'B2B impersonate',
-      resources: [
-        {
-          id: 1046,
-          hasAccess: true,
-          isCustomResource: false,
-        },
-        {
-          id: 1153,
-          hasAccess: true,
-          isCustomResource: false,
-        },
-      ],
-      logins: [],
-      roleType: 0,
-    }
+  // private createRole = () => {
+  //   const data: any = {
+  //     name: 'B2B impersonate',
+  //     resources: [
+  //       {
+  //         id: 1046,
+  //         hasAccess: true,
+  //         isCustomResource: false,
+  //       },
+  //       {
+  //         id: 1153,
+  //         hasAccess: true,
+  //         isCustomResource: false,
+  //       },
+  //     ],
+  //     logins: [],
+  //     roleType: 0,
+  //   }
 
-    console.log('Data to create role =>', data)
+  //   console.log('Data to create role =>', data)
 
-    return this.post(this.routes.createRole(), data)
-      .then((res: any) => {
-        console.log('Saved Role =>', res)
-
-        return res
-      })
-      .catch((err: any) => {
-        console.log('Error saving Role =>', err)
-        throw new Error(err)
-      })
-  }
+  //   return this.post(this.routes.createRole(), data)
+  //     .then((res: any) => {
+  //       console.log('Saved Role =>', res)
+  //       return res
+  //     })
+  //     .catch((err: any) => {
+  //       console.log('Error saving Role =>', err)
+  //       throw new Error(err)
+  //     })
+  // }
 
   public saveUser = async (name: string, email: string) => {
-    // List all roles
-    const roles: any = await this.get(this.routes.getRoles()).catch((err) => {
-      throw new Error(err)
-    })
+    // // List all roles
+    // const roles: any = await this.get(this.routes.getRoles()).catch((err) => {
+    //   throw new Error(err)
+    // })
 
-    let b2brole: any = null
+    // let b2brole: any = null
 
-    // Get only the role "B2B impersonate"
-    b2brole = roles?.items?.find((role: any) => {
-      return role.name === 'B2B impersonate'
-    })
+    // // Get only the role "B2B impersonate"
+    // b2brole = roles?.items?.find((role: any) => {
+    //   return role.name === 'B2B impersonate'
+    // })
 
-    console.log('Role list =>', roles)
+    // console.log('Role list =>', roles)
 
-    // Create this role if it doesn't exists
-    if (!b2brole?.id || b2brole.id === null) {
-      b2brole = await this.createRole()
-    }
+    // // Create this role if it doesn't exists
+    // if (!b2brole?.id || b2brole.id === null) {
+    //   b2brole = await this.createRole()
+    // }
 
-    console.log('b2brole =>', b2brole)
+    // console.log('b2brole =>', b2brole)
 
     const data = {
       email,
       name,
-      roles: [b2brole.id],
+      roles: [957],
     }
 
     // Check if the user exists
@@ -102,17 +99,17 @@ export class LMClient extends ExternalClient {
 
   public deleteUser = async (userId: string) => {
     // List all roles
-    const roles: any = await this.get(this.routes.getRoles())
-    let b2brole: any = null
+    // const roles: any = await this.get(this.routes.getRoles())
+    // let b2brole: any = null
 
-    // Get only the role "B2B impersonate"
-    b2brole = roles?.items?.find((role: any) => {
-      return role.name === 'B2B impersonate'
-    })
-    // Create this role if it doesn't exists
-    if (b2brole?.id) {
-      return this.delete(this.routes.deleteUser(userId, b2brole.id), {})
-    }
+    // // Get only the role "B2B impersonate"
+    // b2brole = roles?.items?.find((role: any) => {
+    //   return role.name === 'B2B impersonate'
+    // })
+    // // Create this role if it doesn't exists
+    // if (b2brole?.id) {
+    return this.delete(this.routes.deleteUser(userId, '957'), {})
+    // }
   }
 
   protected get = <T>(url: string) => {
--- react/components/userNew.tsx
+++ react/components/userNew.tsx
@@ -1,13 +1,21 @@
+/* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { FC } from 'react'
 import React, { useState } from 'react'
 import type { WrappedComponentProps } from 'react-intl'
 import { injectIntl, defineMessages } from 'react-intl'
-import { useQuery, useMutation } from 'react-apollo'
+import { useQuery, useMutation, useLazyQuery } from 'react-apollo'
 import { useRuntime } from 'vtex.render-runtime'
-import { Input, Button, Dropdown, Toggle } from 'vtex.styleguide'
+import {
+  Input,
+  Button,
+  Dropdown,
+  Toggle,
+  AutocompleteInput,
+} from 'vtex.styleguide'
 
 import GET_ROLES from '../queries/ListRoles.gql'
+import SEARCH_USERS from '../queries/SearchUsers.gql'
 import SAVE_USER from '../mutations/saveUser.gql'
 
 const messages = defineMessages({
@@ -36,21 +44,27 @@ const messages = defineMessages({
     defaultMessage: 'Cancel',
   },
   save: {
-    id: 'admin/storefront-permissions.button.delete',
-    defaultMessage: 'Delete',
+    id: 'admin/storefront-permissions.button.save',
+    defaultMessage: 'Save',
   },
 })
+
+let _timeout: any = null
 
 const UserNew: FC<any & WrappedComponentProps> = ({ intl }) => {
   const { navigate } = useRuntime()
   const [state, setState] = useState<any>({
+    id: null,
     roleId: null,
     name: null,
     email: null,
     canImpersonate: false,
   })
 
+  const [term, setTerm] = useState('')
+
   const { loading: loadingRoles, data: dataRoles } = useQuery(GET_ROLES)
+  const [searchUsers, { loading, data: usersData }] = useLazyQuery(SEARCH_USERS)
   const [saveUser, { loading: saveUserLoading }] = useMutation(SAVE_USER, {
     onCompleted: (res: any) => {
       navigate({
@@ -67,6 +81,7 @@ const UserNew: FC<any & WrappedComponentProps> = ({ intl }) => {
   const handleSaveUser = () => {
     saveUser({
       variables: {
+        id: state.id,
         roleId: state.roleId,
         name: state.name,
         email: state.email,
@@ -75,66 +90,145 @@ const UserNew: FC<any & WrappedComponentProps> = ({ intl }) => {
     })
   }
 
+  console.log('usersData =>', usersData)
+
+  const options = {
+    onSelect: (args: any) => {
+      console.log('onSelect: ', args)
+
+      const selectedUser = usersData.users.find((user: any) => {
+        console.log('loop =>', user)
+
+        return user.id === args.value
+      })
+
+      console.log('selectedUser =>', selectedUser)
+
+      const { id, firstName, lastName, email } = selectedUser
+
+      setState({
+        ...state,
+        id,
+        name: `${firstName} ${lastName}`,
+        email,
+      })
+    },
+    loading,
+    value: usersData?.users?.length
+      ? usersData.users.map((user: any) => {
+          console.log('Values =>', {
+            value: user.id,
+            label: `${user.firstName} ${user.lastName}<${user.email}>`,
+          })
+
+          return {
+            value: user.id,
+            label: `${user.firstName} ${user.lastName}<${user.email}>`,
+          }
+        })
+      : [],
+    // --- This is what makes the Last Searched Terms work!
+    // This can be stored anywhere the dev wants. To be persistent, for example.
+  }
+
+  const input = {
+    onChange: (value: any) => {
+      console.log('onChange =>', value)
+      setTerm(value)
+      clearTimeout(_timeout)
+      _timeout = setTimeout(() => {
+        searchUsers({
+          variables: {
+            perPage: 10,
+            pageNumber: 0,
+            filter: `_where=((email=*${term}* OR firstName=*${term}* OR lastName=*${term}*))`,
+          },
+        })
+      }, 1000)
+    },
+    // onSearch: (...args: any) => {
+    //   searchUsers({
+    //     variables: {
+    //       perPage: 10,
+    //       pageNumber: 1,
+    //       filter: term,
+    //     },
+    //   })
+    //   console.log('onSearch:', ...args)
+    // },
+    onClear: () => setTerm(''),
+    placeholder: 'Search user... (e.g.: Ana)',
+    value: term,
+  }
+
   return (
     <div className="w-100 pt6">
       <div className="mb5">
-        <Input
-          label={intl.formatMessage(messages.name)}
-          value={state.name}
-          errorMessage={
-            state.name === '' ? intl.formatMessage(messages.required) : ''
-          }
-          disabled={loadingRoles || saveUserLoading}
-          onChange={(e: any) => {
-            setState({ ...state, name: e.target.value })
-          }}
-        />
+        <AutocompleteInput input={input} options={options} />
       </div>
-
-      <div className="mb5">
-        <Input
-          label={intl.formatMessage(messages.email)}
-          value={state.email}
-          errorMessage={
-            state.email === '' ? intl.formatMessage(messages.required) : ''
-          }
-          disabled={loadingRoles || saveUserLoading}
-          onChange={(e: any) => {
-            setState({ ...state, email: e.target.value })
-          }}
-        />
-      </div>
-
-      <div className="mb5">
-        <Dropdown
-          label={intl.formatMessage(messages.role)}
-          disabled={loadingRoles || saveUserLoading}
-          options={
-            dataRoles?.listRoles?.map((role: any) => {
-              return {
-                value: role.id,
-                label: role.name,
+      {!!state.id && (
+        <>
+          <div className="mb5">
+            <Input
+              label={intl.formatMessage(messages.name)}
+              value={state.name}
+              readOnly={!!state.id}
+              errorMessage={
+                state.name === '' ? intl.formatMessage(messages.required) : ''
               }
-            }) ?? []
-          }
-          value={state.roleId}
-          onChange={(_: any, v: any) => {
-            setState({ ...state, roleId: v })
-          }}
-        />
-      </div>
+              disabled={loadingRoles || saveUserLoading}
+              onChange={(e: any) => {
+                setState({ ...state, name: e.target.value })
+              }}
+            />
+          </div>
 
-      <div className="mb5">
-        <Toggle
-          label={intl.formatMessage(messages.canImpersonate)}
-          size="large"
-          disabled={loadingRoles || saveUserLoading}
-          checked={state.canImpersonate}
-          onChange={() => {
-            setState({ ...state, canImpersonate: !state.canImpersonate })
-          }}
-        />
-      </div>
+          <div className="mb5">
+            <Input
+              label={intl.formatMessage(messages.email)}
+              value={state.email}
+              readOnly={!!state.id}
+              errorMessage={
+                state.email === '' ? intl.formatMessage(messages.required) : ''
+              }
+              disabled={loadingRoles || saveUserLoading}
+              onChange={(e: any) => {
+                setState({ ...state, email: e.target.value })
+              }}
+            />
+          </div>
+          <div className="mb5">
+            <Dropdown
+              label={intl.formatMessage(messages.role)}
+              disabled={loadingRoles || saveUserLoading}
+              options={
+                dataRoles?.listRoles?.map((role: any) => {
+                  return {
+                    value: role.id,
+                    label: role.name,
+                  }
+                }) ?? []
+              }
+              value={state.roleId}
+              onChange={(_: any, v: any) => {
+                setState({ ...state, roleId: v })
+              }}
+            />
+          </div>
+
+          <div className="mb5">
+            <Toggle
+              label={intl.formatMessage(messages.canImpersonate)}
+              size="large"
+              disabled={loadingRoles || saveUserLoading}
+              checked={state.canImpersonate}
+              onChange={() => {
+                setState({ ...state, canImpersonate: !state.canImpersonate })
+              }}
+            />
+          </div>
+        </>
+      )}
 
       <div className="mv4 flex justify-between">
         <Button
@@ -147,18 +241,20 @@ const UserNew: FC<any & WrappedComponentProps> = ({ intl }) => {
         >
           {intl.formatMessage(messages.cancel)}
         </Button>
-        <Button
-          variation="primary"
-          disabled={
-            loadingRoles || saveUserLoading || !state.name || !state.email
-          }
-          collapseRight
-          onClick={() => {
-            handleSaveUser()
-          }}
-        >
-          {intl.formatMessage(messages.save)}
-        </Button>
+        {!!state.id && (
+          <Button
+            variation="primary"
+            disabled={
+              loadingRoles || saveUserLoading || !state.name || !state.email
+            }
+            collapseRight
+            onClick={() => {
+              handleSaveUser()
+            }}
+          >
+            {intl.formatMessage(messages.save)}
+          </Button>
+        )}
       </div>
     </div>
   )
