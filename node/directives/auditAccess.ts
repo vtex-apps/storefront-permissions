300f5095cd247965d99627d9a5530020a058e8c4
Rudge <rudgee@gmail.com>
2023-10-30 22:46:16 -0300

    feat: add audit metrics for authenticated access
    jira: B2BTEAM-1433

---
+++ node/directives/auditAccess.ts
@@ -0,0 +1,62 @@
+import type { GraphQLField } from 'graphql'
+import { defaultFieldResolver } from 'graphql'
+import { SchemaDirectiveVisitor } from 'graphql-tools'
+import sendAuthMetric, {AuthMetric} from "../metrics/auth";
+import {checkUserPermission} from "../resolvers/Queries/Users";
+
+
+export class AuditAccess extends SchemaDirectiveVisitor {
+  public visitFieldDefinition(field: GraphQLField<any, any>) {
+    const { resolve = defaultFieldResolver } = field
+
+    field.resolve = async (
+      root: any,
+      args: any,
+      context: Context,
+      info: any
+    ) => {
+      const {
+        vtex: { adminUserAuthToken, storeUserAuthToken, account, logger },
+        request,
+      } = context
+
+      const operation = field.astNode?.name?.value ?? request.url
+      const forwardedHost = request.headers['x-forwarded-host'] as string
+      const caller = request.headers['x-vtex-caller'] as string
+
+      const hasAdminToken =
+        !!(adminUserAuthToken ??
+        (context?.headers.vtexidclientautcookie as string))
+
+      const hasStoreToken = !!storeUserAuthToken
+      const hasApiToken = !!request.headers['vtex-api-apptoken']
+
+      let role
+      let permissions = []
+
+      if (hasAdminToken || hasStoreToken) {
+        const checkUserPermissions = await checkUserPermission(null,
+          { skipError: true },
+          context
+      )
+        role = checkUserPermissions.role.slug
+        permissions = checkUserPermissions.permissions
+      }
+
+      const authMetric = new AuthMetric(account, {
+        caller,
+        forwardedHost,
+        hasAdminToken,
+        hasApiToken,
+        hasStoreToken,
+        operation,
+        permissions,
+        role,
+      })
+
+      sendAuthMetric(logger, authMetric)
+
+      return resolve(root, args, context, info)
+    }
+  }
+}
---
+++ node/metrics/auth.ts
@@ -0,0 +1,42 @@
+import type { Logger } from '@vtex/api/lib/service/logger/logger'
+import {B2B_METRIC_NAME, Metric, sendMetric} from "../clients/metrics";
+
+
+export interface AuthAuditMetric {
+  operation: string
+  forwardedHost: string
+  caller: string
+  role: string
+  permissions: string[]
+  hasAdminToken: boolean
+  hasStoreToken: boolean
+  hasApiToken: boolean
+}
+
+export class AuthMetric implements Metric {
+  public readonly description: string
+  public readonly kind: string
+  public readonly account: string
+  public readonly fields: AuthAuditMetric
+  public readonly name = B2B_METRIC_NAME
+
+  constructor(account: string, fields: AuthAuditMetric) {
+    this.account = account
+    this.fields = fields
+    this.kind = 'b2b-storefront-permissions-auth-event'
+    this.description = 'Auth metric event'
+  }
+}
+
+const sendAuthMetric = async (logger: Logger, authMetric: AuthMetric) => {
+  try {
+    await sendMetric(authMetric)
+  } catch (error) {
+    logger.error({
+      error,
+      message: `Error to send metrics from auth metric`,
+    })
+  }
+}
+
+export default sendAuthMetric
