2fd71c2204ffb7e4db19e696da1d6598d3383bbc
pabloppupulin <151082530+pabloppupulin@users.noreply.github.com>
2024-03-14 12:20:59 -0300

    Update index.ts

--- node/resolvers/Routes/index.ts
+++ node/resolvers/Routes/index.ts
@@ -377,7 +377,7 @@ export const Routes = {
 
     businessDocument =
       costCenterResponse.data.getCostCenterById.businessDocument?.replace(
-        /\s*/,
+        /[^a-zA-Z0-9]*/,
         ''
       )
 
