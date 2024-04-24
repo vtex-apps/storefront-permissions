270951b153cc5b48a8024ff44ed450621f0270d6
Enzo Mercanti <enzo.mercanti@vtex.com.br>
2024-04-24 14:46:15 -0300

    chore: add token validation logs

--- node/directives/checkAdminAccess.ts
+++ node/directives/checkAdminAccess.ts
@@ -19,15 +19,38 @@ export class CheckAdminAccess extends SchemaDirectiveVisitor {
       } = context
 
       if (!adminUserAuthToken) {
+        logger.warn({
+          message: 'CheckAdminAccess: No admin token provided',
+          userAgent: context.request.header['user-agent'],
+          vtexCaller: context.request.header['x-vtex-caller'],
+          forwardedHost: context.request.header['x-forwarded-host'],
+        })
         throw new AuthenticationError('No token was provided')
       }
 
       try {
-        await identity.validateToken({ token: adminUserAuthToken })
+        const authUser = await identity.validateToken({
+          token: adminUserAuthToken,
+        })
+
+        // This is the first step before actually enabling this code.
+        // For now we only log in case of errors, but in follow up commits
+        // we should also throw an exception inside this if in case of errors
+        if (!authUser?.audience || authUser?.audience !== 'admin') {
+          logger.warn({
+            message: `CheckUserAccess: Token is not an admin token`,
+            userAgent: context.request.header['user-agent'],
+            vtexCaller: context.request.header['x-vtex-caller'],
+            forwardedHost: context.request.header['x-forwarded-host'],
+          })
+        }
       } catch (err) {
         logger.warn({
           error: err,
           message: 'CheckAdminAccess: Invalid token',
+          userAgent: context.request.header['user-agent'],
+          vtexCaller: context.request.header['x-vtex-caller'],
+          forwardedHost: context.request.header['x-forwarded-host'],
           token: adminUserAuthToken,
         })
         throw new ForbiddenError('Unauthorized Access')
--- node/directives/checkUserAccess.ts
+++ node/directives/checkUserAccess.ts
@@ -3,6 +3,8 @@ import type { GraphQLField } from 'graphql'
 import { defaultFieldResolver } from 'graphql'
 import { SchemaDirectiveVisitor } from 'graphql-tools'
 
+import { getActiveUserByEmail } from '../resolvers/Queries/Users'
+
 export async function checkUserOrAdminTokenAccess(
   ctx: Context,
   operation?: string
@@ -15,6 +17,9 @@ export async function checkUserOrAdminTokenAccess(
   if (!adminUserAuthToken && !storeUserAuthToken) {
     logger.warn({
       message: `CheckUserAccess: No admin or store token was provided`,
+      userAgent: ctx.request.header['user-agent'],
+      vtexCaller: ctx.request.header['x-vtex-caller'],
+      forwardedHost: ctx.request.header['x-forwarded-host'],
       operation,
     })
     throw new AuthenticationError('No admin or store token was provided')
@@ -22,11 +27,29 @@ export async function checkUserOrAdminTokenAccess(
 
   if (adminUserAuthToken) {
     try {
-      await identity.validateToken({ token: adminUserAuthToken })
+      const authUser = await identity.validateToken({
+        token: adminUserAuthToken,
+      })
+
+      // This is the first step before actually enabling this code.
+      // For now we only log in case of errors, but in follow up commits
+      // we should also throw an exception inside this if in case of errors
+      if (!authUser?.audience || authUser?.audience !== 'admin') {
+        logger.warn({
+          message: `CheckUserAccess: Token is not an admin token`,
+          userAgent: ctx.request.header['user-agent'],
+          vtexCaller: ctx.request.header['x-vtex-caller'],
+          forwardedHost: ctx.request.header['x-forwarded-host'],
+          operation,
+        })
+      }
     } catch (err) {
       logger.warn({
         error: err,
         message: `CheckUserAccess: Invalid admin token`,
+        userAgent: ctx.request.header['user-agent'],
+        vtexCaller: ctx.request.header['x-vtex-caller'],
+        forwardedHost: ctx.request.header['x-forwarded-host'],
         operation,
       })
       throw new ForbiddenError('Unauthorized Access')
@@ -39,14 +62,51 @@ export async function checkUserOrAdminTokenAccess(
       if (!authUser?.user) {
         logger.warn({
           message: `CheckUserAccess: No valid user found by store user token`,
+          userAgent: ctx.request.header['user-agent'],
+          vtexCaller: ctx.request.header['x-vtex-caller'],
+          forwardedHost: ctx.request.header['x-forwarded-host'],
           operation,
         })
         authUser = null
+      } else {
+        // This is the first step before actually enabling this code.
+        // For now we only log in case of errors, but in follow up commits
+        // we will remove this additional try/catch and set authUser = null
+        // in case of errors
+        try {
+          const user = (await getActiveUserByEmail(
+            null,
+            { email: authUser?.user },
+            ctx
+          )) as { roleId: string } | null
+
+          if (!user?.roleId) {
+            logger.warn({
+              message: `CheckUserAccess: No valid user found by store user token`,
+              userAgent: ctx.request.header['user-agent'],
+              vtexCaller: ctx.request.header['x-vtex-caller'],
+              forwardedHost: ctx.request.header['x-forwarded-host'],
+              operation,
+            })
+          }
+        } catch (err) {
+          logger.warn({
+            error: err,
+            message: `CheckUserAccess: Error getting user by email`,
+            userAgent: ctx.request.header['user-agent'],
+            vtexCaller: ctx.request.header['x-vtex-caller'],
+            forwardedHost: ctx.request.header['x-forwarded-host'],
+            operation,
+          })
+        }
       }
     } catch (err) {
       logger.warn({
         error: err,
         message: `CheckUserAccess: Invalid store user token`,
+        userAgent: ctx.request.header['user-agent'],
+        vtexCaller: ctx.request.header['x-vtex-caller'],
+        forwardedHost: ctx.request.header['x-forwarded-host'],
         operation,
       })
       authUser = null
