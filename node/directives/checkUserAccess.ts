57a2162d650b8cfa9a69afd16a5fe49d9664ccb2
Enzo Mercanti <enzo.mercanti@vtex.com.br>
2024-04-24 11:21:44 -0300

    Revert "fix: add role check on checkUserAccess (#133)"
    This reverts commit b4be44f18c0079744187030aa99951d65edf0d49.

--- node/directives/checkUserAccess.ts
+++ node/directives/checkUserAccess.ts
@@ -3,8 +3,6 @@ import type { GraphQLField } from 'graphql'
 import { defaultFieldResolver } from 'graphql'
 import { SchemaDirectiveVisitor } from 'graphql-tools'
 
-import { getActiveUserByEmail } from '../resolvers/Queries/Users'
-
 export async function checkUserOrAdminTokenAccess(
   ctx: Context,
   operation?: string
@@ -44,20 +42,6 @@ export async function checkUserOrAdminTokenAccess(
           operation,
         })
         authUser = null
-      } else {
-        const user = (await getActiveUserByEmail(
-          null,
-          { email: authUser?.user },
-          ctx
-        )) as { roleId: string } | null
-
-        if (!user?.roleId) {
-          logger.warn({
-            message: `CheckUserAccess: No valid role for user found by store user token`,
-            operation,
-          })
-          authUser = null
-        }
       }
     } catch (err) {
       logger.warn({
