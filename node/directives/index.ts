e801c0b8ff95bddc7ed0abcfc8507bec0d1d16c1
Matheus-Aguilar <matheus.aguilar@vtex.com>
2024-07-04 10:39:07 -0300

    fix: add admin validation directive

--- node/directives/index.ts
+++ node/directives/index.ts
@@ -3,12 +3,14 @@ import { WithSender } from './withSender'
 import { WithUserPermissions } from './withUserPermissions'
 import { CheckAdminAccess } from './checkAdminAccess'
 import { CheckUserAccess } from './checkUserAccess'
+import { ValidateAdminUserAccess } from './validateAdminUserAccess'
 import { ValidateStoreUserAccess } from './validateStoreUserAccess'
 import { AuditAccess } from './auditAccess'
 
 export const schemaDirectives = {
   checkAdminAccess: CheckAdminAccess as any,
   checkUserAccess: CheckUserAccess as any,
+  validateAdminUserAccess: ValidateAdminUserAccess as any,
   validateStoreUserAccess: ValidateStoreUserAccess as any,
   withSession: WithSession,
   withSender: WithSender,
