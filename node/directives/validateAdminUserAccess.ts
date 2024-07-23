82edf64451140d665fedc6f364ee3319ebac2053
Matheus-Aguilar <matheus.aguilar@vtex.com>
2024-07-23 09:47:12 -0300

    fix: check admin token on header

--- node/directives/validateAdminUserAccess.ts
+++ node/directives/validateAdminUserAccess.ts
@@ -117,7 +117,7 @@ export class ValidateAdminUserAccess extends SchemaDirectiveVisitor {
       }
 
       // deny access if no tokens were provided
-      if (!hasAdminToken && !hasApiToken) {
+      if (!hasAdminToken && !hasAdminTokenOnHeader && !hasApiToken) {
         logger.warn({
           message: 'ValidateAdminUserAccess: No token provided',
           ...metricFields,
