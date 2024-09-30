77d13b51dd15ce7828d8ea0ce1d1332c56d4da17
Arthur Bond <arthur.bond@vtex.com>
2024-09-30 13:53:53 -0400

    Revert token validation change

--- node/directives/validateStoreUserAccess.ts
+++ node/directives/validateStoreUserAccess.ts
@@ -122,7 +122,7 @@ export class ValidateStoreUserAccess extends SchemaDirectiveVisitor {
 
       const { hasStoreToken, hasValidStoreToken } = await validateStoreToken(
         context,
-        (storeUserAuthToken as string) ?? (adminUserAuthToken as string)
+        storeUserAuthToken as string
       )
 
       // add store token metrics
