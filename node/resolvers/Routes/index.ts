c383bc6e464fc46514daee8b1748628127dfad16
asattamini <alexandre.sattamini@logicinfo.com>
2025-05-06 10:01:28 -0300

    Update setProfile with selectedPriceTable field

--- node/resolvers/Routes/index.ts
+++ node/resolvers/Routes/index.ts
@@ -3,7 +3,7 @@ import { json } from 'co-body'
 
 import { getRole } from '../Queries/Roles'
 import { getSessionWatcher } from '../Queries/Settings'
-import { getActiveUserByEmail, getUserByEmail } from '../Queries/Users'
+import { getActiveUserByEmail, getUserByEmail, getB2BUserById } from '../Queries/Users'
 import { generateClUser } from './utils'
 import { getUser, setActiveUserByOrganization } from '../Mutations/Users'
 import { toHash } from '../../utils'
@@ -363,10 +363,16 @@ export const Routes = {
     businessName = organization.name
     tradeName = organization.tradeName
 
-    if (organization.priceTables?.length) {
+    // Get the selectedPriceTable from B2B_USER
+
+    const userWithPriceTable = await getB2BUserById(null, {id: user.id}, ctx ) as {selectedPriceTable: string}
+
+    const selectedPriceTable = userWithPriceTable?.selectedPriceTable ? userWithPriceTable?.selectedPriceTable : organization.priceTables.join(',')
+
+    if (selectedPriceTable || organization.priceTables?.length) {
       response[
         'storefront-permissions'
-      ].priceTables.value = `${organization.priceTables.join(',')}`
+      ].priceTables.value = `${selectedPriceTable}`
     }
 
     let facets = [] as any
