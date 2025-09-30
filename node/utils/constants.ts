ab2988fb8783ac439fdd42ece203d71d112cd1cc
Jorge Acosta <jorge.acosta@vtex.com>
2025-09-30 10:53:17 -0400

    added the constants that were removed

--- node/utils/constants.ts
+++ node/utils/constants.ts
@@ -1,3 +1,13 @@
+export const CUSTOMER_SCHEMA_NAME = 'CL'
+export const CUSTOMER_REQUIRED_FIELDS = [
+  'email',
+  'id',
+  'accountId',
+  'accountName',
+  'dataEntityId',
+]
+export const ROLES_VBASE_ID = 'allRolesVbId'
+
 // License Manager constants
 export const B2B_ORGANIZATIONS_PRODUCT_ID = 97
 export const BUYER_ORGANIZATION_VIEW_ROLE = 'buyer_organization_view'
